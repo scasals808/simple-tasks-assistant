69fe7b52709ad36e7e9b2f6e2e53b0efebac17bc
vladimirzinin <VZinin@alfabank.ru>
2026-02-16 17:10:48 +0300

    Init project + Prisma setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  ignorePatterns: ["dist/", "node_modules/"]
+};
